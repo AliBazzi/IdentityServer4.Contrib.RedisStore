187065226cf1a3687ac5cb007212e90dc0a76872
Ali Bazzi <m.ali.bazzi@hotmail.com>
2017-08-22 14:45:06 +0800

    Add AddOperationalStore Extension method to inject Redis store to Identity Server 4 pipeline.

---
+++ IdentityServer4.Contrib.RedisStore/Extensions/IdentityServerRedisBuilderExtensions.cs
@@ -0,0 +1,25 @@
+﻿using IdentityServer4.Contrib.RedisStore.Stores;
+using IdentityServer4.Stores;
+using StackExchange.Redis;
+
+namespace Microsoft.Extensions.DependencyInjection
+{
+    public static class IdentityServerRedisBuilderExtensions
+    {
+        /// <summary>
+        /// Add Redis Store Operational Store.
+        /// </summary>
+        /// <param name="builder"></param>
+        /// <param name="redisConnectionString">Redis Store Connection String</param>
+        /// <param name="db">the number of Db in Redis Instance, default is 0</param>
+        /// <returns></returns>
+        public static IIdentityServerBuilder AddOperationalStore(
+            this IIdentityServerBuilder builder, string redisConnectionString, int db = 0)
+        {
+            builder.Services.AddSingleton(_ => new RedisStoreMultiplexer(redisConnectionString, db));
+            builder.Services.AddScoped<IDatabaseAsync>(_ => _.GetRequiredService<RedisStoreMultiplexer>().GetDatabase());
+            builder.Services.AddTransient<IPersistedGrantStore, PersistedGrantStore>();
+            return builder;
+        }
+    }
+}
