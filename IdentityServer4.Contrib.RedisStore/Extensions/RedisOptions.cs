95f7f37f1f7ce392b1b5c8ee9af7dd90a06b845a
Ryan Child <rchild@makemusic.com>
2018-05-12 08:59:43 -0600

    Battling a _very_ helpful Visual Studio

--- IdentityServer4.Contrib.RedisStore/Extensions/RedisOptions.cs
+++ IdentityServer4.Contrib.RedisStore/Extensions/RedisOptions.cs
@@ -3,71 +3,71 @@ using StackExchange.Redis;
 
 namespace IdentityServer4.Contrib.RedisStore
 {
-	/// <summary>
-	/// Represents Redis general options.
-	/// </summary>
-	public abstract class RedisOptions
-	{
-		/// <summary>
-		///Configuration options objects for StackExchange.Redis Library.
-		/// </summary>
-		public ConfigurationOptions ConfigurationOptions { get; set; }
+    /// <summary>
+    /// Represents Redis general options.
+    /// </summary>
+    public abstract class RedisOptions
+    {
+        /// <summary>
+        ///Configuration options objects for StackExchange.Redis Library.
+        /// </summary>
+        public ConfigurationOptions ConfigurationOptions { get; set; }
 
-		/// <summary>
-		/// Connection String for connecting to Redis Instance.
-		/// </summary>
-		public string RedisConnectionString { get; set; }
+        /// <summary>
+        /// Connection String for connecting to Redis Instance.
+        /// </summary>
+        public string RedisConnectionString { get; set; }
 
-		/// <summary>
-		///The specific Db number to connect to, default is -1.
-		/// </summary>
-		public int Db { get; set; } = -1;
+        /// <summary>
+        ///The specific Db number to connect to, default is -1.
+        /// </summary>
+        public int Db { get; set; } = -1;
 
-		private string _keyPrefix = string.Empty;
+        private string _keyPrefix = string.Empty;
 
-		/// <summary>
-		/// The Prefix to add to each key stored on Redis Cache, default is Empty.
-		/// </summary>
-		public string KeyPrefix
-		{
-			get
-			{
-				return string.IsNullOrEmpty(this._keyPrefix) ? this._keyPrefix : $"{_keyPrefix}:";
-			}
-			set
-			{
-				this._keyPrefix = value;
-			}
-		}
+        /// <summary>
+        /// The Prefix to add to each key stored on Redis Cache, default is Empty.
+        /// </summary>
+        public string KeyPrefix
+        {
+            get
+            {
+                return string.IsNullOrEmpty(this._keyPrefix) ? this._keyPrefix : $"{_keyPrefix}:";
+            }
+            set
+            {
+                this._keyPrefix = value;
+            }
+        }
 
-		private Lazy<IConnectionMultiplexer> _multiplexer =>
-			GetConnectionMultiplexer(RedisConnectionString, ConfigurationOptions);
+        private Lazy<IConnectionMultiplexer> _multiplexer =>
+            GetConnectionMultiplexer(RedisConnectionString, ConfigurationOptions);
 
-		private static Lazy<IConnectionMultiplexer> GetConnectionMultiplexer(string connectionString,
-			ConfigurationOptions options)
-		{
-			return new Lazy<IConnectionMultiplexer>(() =>
-				string.IsNullOrEmpty(connectionString)
-					? ConnectionMultiplexer.Connect(options)
-					: ConnectionMultiplexer.Connect(connectionString));
-		}
+        private static Lazy<IConnectionMultiplexer> GetConnectionMultiplexer(string connectionString,
+            ConfigurationOptions options)
+        {
+            return new Lazy<IConnectionMultiplexer>(() =>
+                string.IsNullOrEmpty(connectionString)
+                    ? ConnectionMultiplexer.Connect(options)
+                    : ConnectionMultiplexer.Connect(connectionString));
+        }
 
-		internal IConnectionMultiplexer Multiplexer => _multiplexer.Value;
-	}
+        internal IConnectionMultiplexer Multiplexer => _multiplexer.Value;
+    }
 
-	/// <summary>
-	/// Represents Redis Operational store options.
-	/// </summary>
-	public class RedisOperationalStoreOptions : RedisOptions
-	{
+    /// <summary>
+    /// Represents Redis Operational store options.
+    /// </summary>
+    public class RedisOperationalStoreOptions : RedisOptions
+    {
 
-	}
+    }
 
-	/// <summary>
-	/// Represents Redis Cache options.
-	/// </summary>
-	public class RedisCacheOptions : RedisOptions
-	{
+    /// <summary>
+    /// Represents Redis Cache options.
+    /// </summary>
+    public class RedisCacheOptions : RedisOptions
+    {
 
-	}
+    }
 }
